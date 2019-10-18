ce4b0d9f5781f201f9c42b09ef5d1dff16a90bd6
Dcordoba <52927577+diegocw@users.noreply.github.com>
2019-10-18 13:12:30 -0500

    instalado dependencias

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomepageComponent } from './componentes/homepage/homepage.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { LoginpageComponent } from './componentes/loginpage/loginpage.component';
+import { PrivadopageComponent } from './componentes/privadopage/privadopage.component';
+import { NotFoundPageComponent } from './componentes/not-found-page/not-found-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginpageComponent,
+    PrivadopageComponent,
+    NotFoundPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
