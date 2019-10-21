8ec4640df16c328557862aafe0bdcabff03c8185
Dcordoba <52927577+diegocw@users.noreply.github.com>
2019-10-21 16:04:08 -0500

    creacion de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './componentes/homepage/homepage.component';
+import {LoginpageComponent} from './componentes/loginpage/loginpage.component';
+import {RegisterComponent} from './componentes/register/register.component';
+import {PrivadopageComponent} from './componentes/privadopage/privadopage.component';
+import {NotFoundPageComponent} from './componentes/not-found-page/not-found-page.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'login', component: LoginpageComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'privado', component: PrivadopageComponent},
+  {path: '**', component: NotFoundPageComponent},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
