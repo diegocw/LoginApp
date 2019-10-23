c8b60c3fac12bf11afef65200cb18740305b6373
Dcordoba <52927577+diegocw@users.noreply.github.com>
2019-10-23 09:32:19 -0500

    firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCxG7vSJ2YXO2q4zxA3ZmJEOxVIqnQJoFs",
+    authDomain: "loginapp-58505.firebaseapp.com",
+    databaseURL: "https://loginapp-58505.firebaseio.com",
+    projectId: "loginapp-58505",
+    storageBucket: "loginapp-58505.appspot.com",
+    messagingSenderId: "448514091200",
+    appId: "1:448514091200:web:a368d60e25702dd7182525"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebaseConfig: {
+    apiKey: "AIzaSyCxG7vSJ2YXO2q4zxA3ZmJEOxVIqnQJoFs",
+    authDomain: "loginapp-58505.firebaseapp.com",
+    databaseURL: "https://loginapp-58505.firebaseio.com",
+    projectId: "loginapp-58505",
+    storageBucket: "loginapp-58505.appspot.com",
+    messagingSenderId: "448514091200",
+    appId: "1:448514091200:web:a368d60e25702dd7182525"
+  }
 };
 
 /*
